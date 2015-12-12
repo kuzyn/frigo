5e1192a3b1e9bfeca3f53816f875b93cc28e4435
kuzyn <hello@samuelcousin.com>
2015-12-12 01:51:43 +0000

    chore: base/build/gulp :zzz:

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var usersCounter = 0;
+var appTitle = "icebox";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: appTitle, users: usersCounter});
 });
 
 module.exports = router;
